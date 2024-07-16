5319ea9c6a735fa500883b8e7e9044df94d8081a
Lucas Ferreira <lucsferreira.dev@gmail.com>
2024-07-16 03:08:10 -0400

    tests: add some testing for users service

--- src/modules/api/users/users.service.spec.ts
+++ src/modules/api/users/users.service.spec.ts
@@ -1,14 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaModule } from 'src/modules/shared/prisma/prisma.module';
+import { PrismaService } from 'src/modules/shared/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 import { RolesModule } from '../roles/roles.module';
 
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+jest.mock('bcrypt', () => ({
+  hashSync: jest.fn().mockReturnValue('hashedPassword'),
+}));
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prismaService: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule, RolesModule],
       providers: [UsersService],
@@ -19,5 +27,41 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  beforeEach(() => {
+    prismaService = {
+      users: {
+        create: jest.fn().mockResolvedValue(null),
+      },
+    } as unknown as PrismaService;
+
+    service = new UsersService(prismaService);
+  });
+
+  describe('create', () => {
+    it('should create a user when roleId is a positive number', async () => {
+      const user: CreateUserDto = {
+        email: 'test@example.com',
+        name: 'Test User',
+        password: 'password123',
+        roleId: 1, // Valid roleId
+      };
+
+      await expect(service.create(user)).resolves.not.toThrow();
+    });
+
+    it('should hash the password before creating a user', async () => {
+      const user: CreateUserDto = {
+        email: 'test@example.com',
+        name: 'Test User',
+        password: 'password123',
+        roleId: 1,
+      };
+
+      await service.create(user);
+
+      expect(bcrypt.hashSync).toHaveBeenCalledWith(user.password, 10);
+    });
   });
 });
--- src/modules/api/users/users.service.ts
+++ src/modules/api/users/users.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
 
 import { PrismaService } from '../../shared/prisma/prisma.service';
